8308c27af268529b963f8ce2445e5ab4af3ff479
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-07-05 13:17:55 +0100

    feat: Added chatbox to fongit

--- fongit/scripts.js
+++ fongit/scripts.js
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
   let memoryID = Math.floor(new Date().getTime() / 1000);
 
   const newTextRef = document.querySelector(".chat-box");
+  const sendButton = document.querySelector("#send-button");
+  const sendIcon = document.querySelector("#button-send-icon");
+  const loadingIcon = document.querySelector("#button-send-icon-loading");
   let autoScroll = true;
   const currentHeight = { current: 100 };
 
@@ -35,23 +38,22 @@ document.addEventListener("DOMContentLoaded", function () {
         .map((message) => {
           if (message.sender === "user") {
             return `
-            <div id="js-message-user" class="flex w-full justify-end"><div id="js-messaage-user-content" class="relative max-w-[70%] rounded-3xl bg-[#f4f4f4] px-5 py-2.5 dark:bg-token-main-surface-secondary">${message.text}</div></div>  
+            <div id="js-message-user" class="flex w-full justify-end"><div id="js-messaage-user-content" class="relative max-w-[70%] rounded-3xl bg-[#f4f4f4] px-5 py-2.5 dark:bg-token-main-surface-secondary">${message.text}</div></div>
             `;
           } else {
             return `
-            <div id="js-message-ai" class="flex-start flex gap-3 text-base juice:gap-4 juice:md:gap-5 juice:lg:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]"><div class="flex-shrink-0 flex flex-col relative items-end"><div class="border border-gray-300 rounded-full overflow-hidden p-1 mt-2.5"><img src="https://starlingtrust.com/libreria/imagenes/preloader.gif?v=2" class="h-8 w-8"></div></div><div id="js-message-ai-content" class="bg-white relative flex w-full min-w-0 flex-col agent-turn">
-            ${message.text}
-    </div></div>  
+            <div id="js-message-ai" class="flex-start leading-5 text-[17px] flex gap-3 text-base juice:gap-4 juice:md:gap-5 juice:lg:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]"><div class="flex-shrink-0 flex flex-col relative items-end"><div class="-mt-2.5 rounded-full overflow-hidden p-1 "><img src="https://pbs.twimg.com/profile_images/1369223787074445314/K8UkNVMV_400x400.jpg" class="h-8 w-8"></div></div><div id="js-message-ai-content" class="bg-white relative flex w-full min-w-0 flex-col agent-turn">${message.text}</div></div>  
             `;
           }
         })
         .join("")
-      : `<div id="js-chat-start" class="text-center m-auto"><img src="https://starlingtrust.com/libreria/imagenes/preloader.gif?v=2" class="h-20 w-20 mx-auto"><div><h2 class="text-3xl">Welcome to Starling Advisor AI</h2><p> Ask me anything about Starling, and I'll provide you with the information you need!</p></div></div>`;
+      : `<div id="js-chat-start" class="text-center m-auto"><img src="https://pbs.twimg.com/profile_images/1369223787074445314/K8UkNVMV_400x400.jpg" class="h-20 w-20 mx-auto"><div><h2 class="text-3xl">Welcome to Fongit Advisor AI</h2><p> Ask me anything about Fongit, and I'll provide you with the information you need!</p></div></div>`;
   }
 
   function sendPrompt(user_input) {
     if (user_input.trim() === "") return;
     loading = true;
+    toggleLoadingState(true);
 
     if (messages) {
       let num = messages.length;
@@ -62,14 +64,20 @@ document.addEventListener("DOMContentLoaded", function () {
     updateMessages();
     scrollDomToBottom("user");
 
-    fetch("https://starling-api.fly.dev/chat/gf", {
+    
+    // https://starling-api.fly.dev/chat
+    // https://true-response.fly.dev/api/v1/chat
+    // http://localhost:8000/api/v1/chat
+    fetch("https://true-response.fly.dev/api/v1/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ memory_id: memoryID, user_input: user_input })
+      body: JSON.stringify({ memory_id: memoryID, request: user_input })
     })
       .then((response) => response.json())
       .then((data) => {
-        let links = data.source_nodes;
+        console.log(data.metadata)
+        let links = data.metadata.map(item => item.filename)
+        console.log(links)
         response = data.response + "\n\nReferences:\n\n" + links.join("\n\n");
         messages.push({ id: messages.length, text: response, sender: "bot" });
         updateMessages();
@@ -80,7 +88,24 @@ document.addEventListener("DOMContentLoaded", function () {
         messages.push({ id: messages.length, text: errorMessage, sender: "bot" });
         updateMessages();
       })
-      .finally(() => (loading = false));
+      .finally(() => {
+        loading = false;
+        toggleLoadingState(false);
+      });
+  }
+
+  function toggleLoadingState(isLoading) {
+    if (isLoading) {
+      sendIcon.classList.add("hidden");
+      loadingIcon.classList.remove("hidden");
+      sendButton.disabled = true;
+      sendButton.classList.add("opacity-50", "cursor-not-allowed");
+    } else {
+      sendIcon.classList.remove("hidden");
+      loadingIcon.classList.add("hidden");
+      sendButton.disabled = false;
+      sendButton.classList.remove("opacity-50", "cursor-not-allowed");
+    }
   }
 
   document.querySelector("#user-input").addEventListener("keydown", function (e) {
@@ -92,7 +117,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  document.querySelector("#send-button").addEventListener("click", function () {
+  sendButton.addEventListener("click", function () {
     let user_input = document.querySelector("#user-input").value;
     document.querySelector("#user-input").value = "";
     if (!loading) sendPrompt(user_input);
